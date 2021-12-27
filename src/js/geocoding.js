92010294b9324b2ebd84de8335b8277791427069
kai66w <kai.weinmann@freenet.de>
2021-12-27 12:39:22 +0100

    fixed geocoding.js

--- src/js/geocoding.js
+++ src/js/geocoding.js
@@ -1,22 +1,25 @@
-function ReverseGeocoding(lon, lat){
-  fetch('https://nominatim.openstreetmap.org/reverse?format=json&lon' + lon + '&lat' + lat)
+export function ReverseGeocoding(lon, lat){
+  let location = fetch('https://nominatim.openstreetmap.org/reverse?format=json&lon=' + lon + '&lat=' + lat)
     .then(function(response){
       return response.json()
     })
     .then(function(json){
-      return json
+      return json.address
     })
+  return location
 }
 
-function Geocoding(searchstring){
-  fetch('https://nominatim.openstreetmap.org/search?format=json&q=' + searchstring)
+export function Geocoding(searchstring){
+  let coords = fetch('https://nominatim.openstreetmap.org/search?format=json&q=' + searchstring)
     .then(function(response){
       return response.json()
     })
     .then(function(json){
-      return json
+      let coords_lat = json[0].lat
+      let coords_lon = json[0].lon
+      let coords_json = {'lat':coords_lat, 'lon':coords_lon}
+      return coords_json
     })
-
-
+  return coords
 }
 
