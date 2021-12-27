0d7f6631a9f1fb768be04cff217d27d7bdeedf2a
m4nu3l-00 <61203017+m4nu3l-00@users.noreply.github.com>
2021-12-27 11:49:33 +0100

    Added Geocoding functions
    ReverseGeocoding to convert from lon, lat to adress
    GeoCoding to convert from adress to lon, lat

---
+++ src/js/geocoding.js
@@ -0,0 +1,25 @@
+function ReverseGeocoding(lon, lat){
+    fetch('https://nominatim.openstreetmap.org/reverse?format=json&lon' + lon + '&lat' + lat)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(json){
+            return json
+        })
+}
+
+
+
+
+function Geocoding(searchstring){
+    fetch('https://nominatim.openstreetmap.org/search?format=json&q=' + searchstring)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(json){
+            return json
+        })
+
+
+}
+
