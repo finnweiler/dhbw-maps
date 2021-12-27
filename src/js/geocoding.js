25f41b65528c6fa14ae1a78f996d53bd5213057c
kai66w <kai.weinmann@freenet.de>
2021-12-27 11:55:19 +0100

    fixed code style of geocoding.js

--- src/js/geocoding.js
+++ src/js/geocoding.js
@@ -1,24 +1,21 @@
 function ReverseGeocoding(lon, lat){
-    fetch('https://nominatim.openstreetmap.org/reverse?format=json&lon' + lon + '&lat' + lat)
-        .then(function(response){
-            return response.json()
-        })
-        .then(function(json){
-            return json
-        })
+  fetch('https://nominatim.openstreetmap.org/reverse?format=json&lon' + lon + '&lat' + lat)
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(json){
+      return json
+    })
 }
 
-
-
-
 function Geocoding(searchstring){
-    fetch('https://nominatim.openstreetmap.org/search?format=json&q=' + searchstring)
-        .then(function(response){
-            return response.json()
-        })
-        .then(function(json){
-            return json
-        })
+  fetch('https://nominatim.openstreetmap.org/search?format=json&q=' + searchstring)
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(json){
+      return json
+    })
 
 
 }
