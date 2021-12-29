399e680eaf5fa715141668f7bcd7d623bb9924fd
kai66w <kai.weinmann@freenet.de>
2021-12-29 22:30:27 +0100

    let map fly to current location if gps is allowed

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,54 +1,57 @@
 import React, { useState, useEffect } from 'react'
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
 import Routing from './RoutingMachine'
 
+class Map extends React.Component{
+  //the default location is the dhbw friedrichshafen
+  state = {
+    current_state: 'default',
+    map: null,
+    lat: 47.665753037254085,
+    lng: 9.447255091829561
+  }
 
-const Map = () => {
-  let [userPosition, setUserPosition] = useState(null)
-
-  const getUserLocation = () => {
+  getUserLocation(){
     // https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/Using_geolocation
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
-        let coords = {lat: position.coords.latitude, lng: position.coords.longitude}
-        setUserPosition(coords)
+        //seting the state automatically refreshes the component
+        this.setState({
+          current_state: 'gps',
+          map: null,
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        })
+        console.log('set new state')
       })
     } else {
       // Browser doesn't support Geolocation
       alert('Dein Browser unterstützt Geolocation nicht.')
-    }
+    } 
   }
 
-  useEffect(() => {
-    getUserLocation()
-  })
-  if(userPosition === null){
-    return(
-        <MapContainer 
+  render() {
+    console.log('render')
+    //get user location if currently the default location is used
+    if (this.state.current_state == 'default'){
+      this.getUserLocation()
+    }
+    //fly to current location if the map component is set and the gps location is set
+    else if (this.map != null){
+      this.map.flyTo([this.state.lat, this.state.lng], 14)
+    }
+    //return map container
+    return (
+      <div>
+        {<MapContainer 
           zoom={14}
           minZoom={4}
           scrollWheelZoom={true}
-          center={[47.665753037254085, 9.447255091829561]}
-          whenCreated={(map) => { setTimeout(() => map.invalidateSize(), 1000) }}
-          style={{height: '100vh'}}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-
-          <Routing user={userPosition}></Routing>
-        </MapContainer>
-    )
-  } else {
-  return (
-    <div>
-      {<MapContainer 
-          zoom={14}
-          minZoom={4}
-          scrollWheelZoom={true}
-          center={[userPosition.lat, userPosition.lng]}
-          whenCreated={(map) => { setTimeout(() => map.invalidateSize(), 1000) }}
+          center={[this.state.lat, this.state.lng]}
+          whenCreated={(lmap) => {
+            setTimeout(() => lmap.invalidateSize(), 1000)
+            this.map = lmap
+          }}
           style={{height: '100vh'}}
         >
           <TileLayer
@@ -56,11 +59,12 @@ const Map = () => {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           
-          <Routing user={userPosition}></Routing>
+          <Routing user={{lat: this.state.lat, lng: this.state.lng}}></Routing>
         </MapContainer>
-      }
-    </div>
-  )}
+        }
+      </div>
+    )
+  }
 }
 
 export default Map
