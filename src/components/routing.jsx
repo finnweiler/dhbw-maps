40436d6a0e22355d813d02c155de2aa5c79c0d74
Moritz Gärtner <moritzgaertner516@gmail.com>
2022-01-04 11:12:06 +0100

    finally rename routing + use store

--- src/components/routing.jsx
+++ src/components/routing.jsx
@@ -1,26 +1,23 @@
+import { useStore } from 'framework7-react'
 import L from 'leaflet'
 import 'leaflet-routing-machine'
 import { useMapEvents, useMap } from 'react-leaflet'
 
-let last = null
-let instructions = null
+import store from '../js/store'
+
 
 const Routing = (props) => {
   const map = useMap()
+  const previousControl = useStore('routeControl')
 
   useMapEvents({
     click(e) {
 
-      // Warum geht das nur hier oben?
-      instructions = last?._selectedRoute?.instructions
-
-      if (last) {map.removeControl(last)}
-
-      last = L.Routing.control({
+      if (previousControl) { map.removeControl(previousControl) }
+    
+      let control = L.Routing.control({
         language: 'de',
-        formatter:  new L.Routing.Formatter({
-          language: 'de'
-        }),
+        formatter:  new L.Routing.Formatter({ language: 'de' }),
         waypoints: [L.latLng(props.user.lat, props.user.lng), L.latLng(e.latlng.lat, e.latlng.lng)],
         createMarker: function (i, waypoint, n) {
           let marker = null
@@ -40,7 +37,7 @@ const Routing = (props) => {
                 iconSize: [100, 95],
                 iconAnchor: [22, 94],
               })
-            });
+            })
           } else {
             marker = L.marker(waypoint.latLng, {
               draggable: true,
@@ -57,40 +54,23 @@ const Routing = (props) => {
                 iconSize: [38, 95],
                 iconAnchor: [22, 94],
               })
-            });
+            })
           }
 
-          return marker;
+          return marker
         }
       }).addTo(map)
 
-      // Load Instructions in Storage
-      loadInstructions()
+      store.dispatch('newRouteControl', control)
 
+      control.on('routesfound', (e) => {
+        store.dispatch('newRoute', e.routes[0])
+      })
     }
   })
-
+    
   return null
 
 }
-
-function loadInstructions() {
-  let routeItem = last?._selectedRoute
-  if (routeItem != null) {
-    let name = routeItem.name
-    let summary = routeItem.summary
-    let instructions = routeItem.instructions
-
-    let routeObject = {
-      name: name,
-      summary: summary,
-      instructions: instructions
-    }
-    console.log(routeObject)
-  } else {
-    setTimeout(loadInstructions, 100);
-  }
-}
-
 
 export default Routing
