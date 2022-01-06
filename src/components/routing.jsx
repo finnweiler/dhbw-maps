495e95cdef05dc183c362194727363312e9af29f
Niklas <dev@corusm.de>
2022-01-06 23:39:43 +0100

    real fix without timeouts

--- src/components/routing.jsx
+++ src/components/routing.jsx
@@ -10,20 +10,17 @@ const Routing = (props) => {
   const map = useMap()
   const previousControl = useStore('routeControl')
 
-  const destination = useStore('address')
-
-  function loadRoute() {
+  function loadRoute(latlng) {
     const control = L.Routing.control({
       language: 'de',
       formatter:  new L.Routing.Formatter({ language: 'de' }),
-      waypoints: [L.latLng(props.user.lat, props.user.lng), L.latLng(destination.lat, destination.lng)],
+      waypoints: [L.latLng(props.user.lat, props.user.lng), L.latLng(latlng.lat, latlng.lng)],
       fitSelectedRoutes: true,
       addWaypoints: false,
-      routeWhileDragging: false,
+      routeWhileDragging: true,
       createMarker: (i, waypoint, n) => {
         let marker = null
         if (i == 0) {
-          console.log('first',i, n)
           marker = L.marker(waypoint.latLng, {
             draggable: false,
             icon: L.icon({
@@ -33,7 +30,6 @@ const Routing = (props) => {
             })
           })
         } else {
-          console.log('second',i, n)
           marker = L.marker(waypoint.latLng, {
             draggable: true,
             icon: L.icon({
@@ -43,33 +39,31 @@ const Routing = (props) => {
             })
           })
           marker.on('dragend', (e) => {
-            setTimeout(() => {
-              store.dispatch('newAddress', e.target._latlng)
-            }, 500)
-          }) 
+            store.dispatch('newAddress', e.target._latlng)
+          })
         }
         return marker
       }
     }).addTo(map)
 
+
     store.dispatch('newRouteControl', control)
 
     control.on('routesfound', (e) => {
+      console.log(e)
       store.dispatch('newRoute', e.routes[0])
     })
   }
-
-  useEffect(() => {
-    if (destination != null && props.user != null) {
-      if (previousControl) { map.removeControl(previousControl) }
-      loadRoute()
-    }
-  }, [destination, props.user])
 
 
   useMapEvents({
     click(e) {
       store.dispatch('newAddress', e.latlng)
+      
+      if (e.latlng != null && props.user != null) {
+        if (previousControl) { map.removeControl(previousControl) }
+        loadRoute(e.latlng)
+      }
     }
   })
     
