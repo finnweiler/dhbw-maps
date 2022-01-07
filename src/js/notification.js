de73f5be9c76972d74f3c1188edb685665ab0145
m4nu3l-00 <61203017+m4nu3l-00@users.noreply.github.com>
2022-01-07 11:41:14 +0100

    Communication fix

--- src/js/notification.js
+++ src/js/notification.js
@@ -1,6 +1,7 @@
 self.addEventListener('notificationclick', function (event) {
   event.notification.close()
-  self.clients.matchAll(options).then(function (clients) {
+  self.clients.matchAll({includeUncontrolled: true}).then(function (clients) {
+    console.log(clients, clients.length)
     if (clients && clients.length) {
       //Respond to last focused tab
       clients[0].postMessage({type: 'notification_geolocation'});
